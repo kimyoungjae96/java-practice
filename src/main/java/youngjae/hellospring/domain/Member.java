e810217fcb3a83961cc0f9edbdf843b9e27701fe
kimyoungjae <sskim5421@gmail.com>
2021-10-11 18:53:16 +0900

    Add member repository

---
+++ src/main/java/youngjae/hellospring/domain/Member.java
@@ -0,0 +1,18 @@
+package youngjae.hellospring.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
